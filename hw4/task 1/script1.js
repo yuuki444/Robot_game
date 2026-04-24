112398b8899d6d77d11fa174e029740790778106
yuuki <academymansur123@gmail.com>
2026-04-24 09:27:50 +0500

    DOM

---
+++ hw4/task 1/script1.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const texts = document.getElementsByClassName("text");
+const paragraphs = document.getElementsByTagName("p");
+const firstBtn = document.querySelector("button");
+const allP = document.querySelectorAll("p");
+
+console.log(title);
+console.log(texts);
+console.log(paragraphs);
+console.log(firstBtn);
+console.log(allP);
